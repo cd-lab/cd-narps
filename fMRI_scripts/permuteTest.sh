598cab3ebe4db1de90db3f7074d73f46b9e0ad86
Claudio Toro <ctoro@bu.edu>
2019-02-14 12:14:04 -0500

    script to run a one-sample perm test over F-thresholded clustering

---
+++ fMRI_scripts/permuteTest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+# load modules within SCC
+module load fsl
+module list
+
+ID=$1
+INPUT=${ID}/glm04_4D_sm4mm_mask85_${ID}.nii.gz
+OUTPUT=${ID}/glm04_sm4mm_mask85_oneSamp_${ID}
+MASK=mask_thresh_85.nii.gz
+
+echo "Processing analysis ${ID}..."
+
+# perform one-sample t-test
+# -x indicates to output both correct and uncorrected maps
+randomise -i $INPUT -o $OUTPUT -1 -x -F 10.69 -m $MASK -v 5
+
+
+echo "Done."
