9c352f16c1b2bc82b28b2b8d344b4a110ebd57bf
Claudio Toro <ctoro@bu.edu>
2019-02-15 13:38:21 -0500

    forgot to specify group-specific sample sizes

--- fMRI_scripts/permuteTest.sh
+++ fMRI_scripts/permuteTest.sh
@@ -5,6 +5,7 @@ module load fsl
 module list
 
 ID=$1
+GROUP=$(echo $ID | cut -f 2 -d "_")
 INPUT=${ID}/glm04_4D_sm4mm_mask85_${ID}.nii.gz
 OUTPUT=${ID}/glm04_sm4mm_mask85_oneSamp_${ID}
 MASK=mask_thresh_85.nii.gz
@@ -13,7 +14,7 @@ echo "Processing analysis ${ID}..."
 
 # perform one-sample t-test
 # -x indicates to output both correct and uncorrected maps
-randomise -i $INPUT -o $OUTPUT -S 10.69 -m $MASK -v 5 -d design102.mat -t design.con -f design.fts -x 
+randomise -i $INPUT -o $OUTPUT -S 10.69 -m $MASK -v 5 -d design_${GROUP}.mat -t design.con -f design.fts -x 
 
 
 echo "Done."
