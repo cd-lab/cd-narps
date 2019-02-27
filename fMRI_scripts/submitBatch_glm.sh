11072a08af8885e7c21fe3c2efdb3b8e4ece8bdd
Claudio Toro-Serey <ctoro@bu.edu>
2019-02-27 13:38:37 -0500

    small updates to individual glms so trials are run with impulse responses

--- fMRI_scripts/submitBatch_glm.sh
+++ fMRI_scripts/submitBatch_glm.sh
@@ -5,10 +5,10 @@ batch=$1
 
 # for each subj id in batch, run the GLM if it hasn't been done before
 cat $batch | while read line || [ -n "$line" ]; do \
-    if [ ! -f ./output/glm004_subject${line}.nii.gz ] ; then
+    if [ ! -f ./output/glm005_subject${line}.nii.gz ] ; then
         echo "Subj $line is running..."
-        qsub glm4.sh $line -P cd-narps
+        qsub glm5.sh $line -P cd-narps
     else
-        echo "GLM for participant ${line} already exists..."
+        echo "GLM for participant already exists..."
     fi ; \
 done
