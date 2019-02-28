7c17bcbf2ddbef4a5e226e1918c379db112147a3
Claudio Toro-Serey <ctoro@bu.edu>
2019-02-28 15:02:15 -0500

    updated permutation script to operate on glm5 data

--- fMRI_scripts/permuteTest.sh
+++ fMRI_scripts/permuteTest.sh
@@ -8,8 +8,8 @@ ID=$1 # dir name that contains the data and design files
 THRESH=${2:-1.97} # threshold for clustermass (defaults to 1.97)
 
 GROUP=$(echo $ID | cut -f 2 -d "_")
-INPUT=${ID}/glm04_4D_sm4mm_mask85_${ID}.nii.gz
-OUTPUT=${ID}/glm04_sm4mm_mask85_oneSamp_${ID}
+INPUT=${ID}/glm05_4D_sm4mm_mask85_${ID}.nii.gz
+OUTPUT=${ID}/glm05_sm4mm_mask85_oneSamp_${ID}
 MASK=mask_thresh_85.nii.gz
 DESIGN=${ID}/design.mat
 CONTRAST=${ID}/contrast.con
